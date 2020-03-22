2b2446fa5688882a54f553e178d5c5c1c0696243
bignikmak <bignikmak@gmail.com>
2020-03-22 20:28:23 +0200

    inita commit

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home2(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+
+    @GetMapping("/aboutus")
+    public String aboutus(Model model) {
+        model.addAttribute("title", "About us");
+        return "about";
+    }
+
+}
