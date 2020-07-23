74e6a909d593b1c540b62812dc57307e1d41a292
bignikmak <bignikmak@gmail.com>
2020-07-23 08:34:04 +0300

    new site

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -8,15 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String home2(Model model) {
-        model.addAttribute("title", "Main Page");
+    public String home(Model model) {
+        model.addAttribute("title", "Логово батраков");
         return "home";
     }
 
-    @GetMapping("/aboutus")
+
+
+    @GetMapping("/challonge")
+    public String challonge(Model model) {
+        model.addAttribute("title", "Турниры");
+        return "challonge";
+    }
+
+
+    @GetMapping("/about-us")
     public String aboutus(Model model) {
-        model.addAttribute("title", "About us");
+        model.addAttribute("title", "О нас");
         return "about";
+    }
+
+    @GetMapping("/donations")
+    public String donations(Model model) {
+        model.addAttribute("title", "Поддержка");
+        return "donate";
+    }
+
+    @GetMapping("/license")
+    public String license(Model model) {
+        model.addAttribute("title", "Поддержка");
+        return "license";
     }
 
 }
